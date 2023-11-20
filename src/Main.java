a1b490c2a1600160ec5143e75f3720ccebfc10f7
jwhougland <47694635+jwhougland@users.noreply.github.com>
2023-11-20 10:25:16 -0600

    Calling upon the Employee and Address classes to create an aggregation demo

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,44 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
+/**
+ * Main class for the aggregation example console app.
+ */
 public class Main {
+    /**
+     * Entry point for the aggregation example console app.
+     * @param args Not used.
+     */
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+        // Create an address
+        Address address = new Address(
+                "1234 Sample Street",
+                "Arlington",
+                Address.USState.TEXAS,
+                "75104");
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+        // Print the address
+        System.out.println("Printing the address before creating an employee instance:");
+        System.out.println(address);
+        System.out.println();
+
+        runEmployeeAddressAggregationDemo(address);
+
+        System.out.println("Printing the address again after the demo method has finished:");
+        System.out.println(address);
+    }
+
+    /**
+     * Creates an employee instance locally using the given data
+     *
+     * @param address Address to assign to the local employee instance
+     */
+    public static void runEmployeeAddressAggregationDemo(Address address) {
+
+        // Create an employee instance
+        Employee employee = new Employee("Peter Gibbons", Employee.JobRole.SENIOR_SOFTWARE_ENGINEER, address);
+
+        // Print out the employee's details
+        System.out.println("Printing the employee's details:");
+        System.out.println(employee);
+        System.out.println();
     }
 }
